11d4229c29ac92d2ab7127bdcafe8a376719175b
Primalpat- <r.patrickwilliams@gmail.com>
2017-11-30 09:32:31 -0500

    Started adding some timeout handling

--- Zelus.Web/Models/Scrapers/CollectionScraper.cs
+++ Zelus.Web/Models/Scrapers/CollectionScraper.cs
@@ -14,19 +14,11 @@ namespace Zelus.Web.Models.Scrapers
         public List<PlayerCharacter> Execute(ZelusDbContext db)
         {
             var results = new List<PlayerCharacter>();
-            var web = new HtmlWeb();
             var players = db.Players.ToList();
 
             foreach (var player in players)
             {
-                var playerCollection = web.Load($"{player.SwgohGgUrl}collection/");
-                var characterContainers = playerCollection.DocumentNode
-                                                          .SelectSingleNode("//*[contains(concat(\" \", normalize-space(@class), \" \"), \" collection-char-list \")]")
-                                                          .Element("div")                                                //Get the child row
-                                                          .Elements("div")                                               //Get the col children
-                                                          .Select(e => e.Element("div"))                                 //Select the collection-char
-                                                          .Where(d => !d.Attributes["class"].Value.Contains("missing"))  //Filter out the locked characters
-                                                          .ToList();
+                var characterContainers = GetCharacterNodes(player);
 
                 foreach (var container in characterContainers)
                 {
@@ -36,6 +28,53 @@ namespace Zelus.Web.Models.Scrapers
             }
 
             return results;
+        }
+
+        private List<HtmlNode> GetCharacterNodes(Player player)
+        {
+            try
+            {
+                var web = new HtmlWeb();
+                var characterContainers = new List<HtmlNode>();
+                var sleepTimer = 1000;
+                while (characterContainers.Count == 0)
+                {
+                    var playerCollection = GetPlayerCollection(player);
+                    characterContainers = playerCollection.DocumentNode?
+                                                          .SelectSingleNode("//*[contains(concat(\" \", normalize-space(@class), \" \"), \" collection-char-list \")]")?
+                                                          .Element("div")?                                                //Get the child row
+                                                          .Elements("div")?                                               //Get the col children
+                                                          .Select(e => e.Element("div"))?                                 //Select the collection-char
+                                                          .Where(d => !d.Attributes["class"].Value.Contains("missing"))?  //Filter out the locked characters
+                                                          .ToList();
+
+                    System.Threading.Thread.Sleep(sleepTimer);
+                    if (sleepTimer <= 30000)
+                        sleepTimer += 1000;
+                }
+
+                return characterContainers;
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("Failed when retrieving character nodes");
+            }
+        }
+
+        private HtmlDocument GetPlayerCollection(Player player)
+        {
+            var web = new HtmlWeb();
+            var playerCollection = web.Load($"{player.SwgohGgUrl}collection/");
+
+            var sleepTimer = 1000;
+            while (playerCollection == null || playerCollection.DocumentNode.ChildNodes.Count == 0)
+            {
+                System.Threading.Thread.Sleep(sleepTimer);
+                playerCollection = web.Load($"{player.SwgohGgUrl}collection/");
+                if (sleepTimer <= 30000)
+                    sleepTimer += 1000;
+            }
+            return playerCollection;
         }
     }
 }
